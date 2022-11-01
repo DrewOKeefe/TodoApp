1b027ee6ec7607cdb67b9fb0298704ec3aceb420
Drew O'Keefe <okeefedrew@gmail.com>
2022-11-01 13:14:02 -0500

    working app

--- 4.4TodoExercise.js
+++ 4.4TodoExercise.js
@@ -8,6 +8,7 @@ todoList.addEventListener('click', function (e) {
 	if (e.target.tagName === 'LI') {
 		e.target.classList.toggle('completed');
 	}
+	localStorage.setItem('savedTodos', JSON.stringify(todoList.innerHTML));
 });
 
 //CREATING NEW LI WITH BUTTONS:
@@ -15,28 +16,39 @@ form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
 	if (input.value) {
-	//NEW LI:
-	let newTodo = document.createElement('li');
-	let taskValue = document.getElementById('task').value;
-	newTodo.innerText = taskValue;
-	input.value = '';
-	todoList.appendChild(newTodo);
+		//NEW LI:
+		let newTodo = document.createElement('li');
+		let taskValue = document.getElementById('task').value;
+		newTodo.innerText = taskValue;
+		input.value = '';
+		todoList.appendChild(newTodo);
 
 		//NEW DELETE BUTTON:
 		let deleteBtn = document.createElement('button');
 		deleteBtn.innerText = 'Delete';
 		newTodo.appendChild(deleteBtn);
 		deleteBtn.addEventListener('click', function (e) {
-			// console.log("asdfsd")
+			// console.log('hello');
 			e.target.parentElement.remove();
 		});
 	} else alert('Please enter task');
+});
 
-	//SAVE TO LOCAL STORAGE:
-	console.log(todoList.innerHTML);
-	console.log(JSON.stringify(todoList.innerHTML));
-	localStorage.setItem('savedTodos', todoList.innerHTML);
+// SAVED TO LOCAL STORAGE:
+document.addEventListener('click', function () {
+	localStorage.setItem('savedTodos', JSON.stringify(todoList.innerHTML));
+});
+
+document.addEventListener('submit', function () {
+	localStorage.setItem('savedTodos', JSON.stringify(todoList.innerHTML));
 });
 
 //RETRIEVING FROM LOCAL STORAGE:
-JSON.parse(localStorage.getItem('todos'));
+todoList.innerHTML = JSON.parse(localStorage.savedTodos);
+
+todoList.addEventListener('click', function (e) {
+	if (e.target.tagName === 'BUTTON') {
+		// console.log("loaded hello")
+		e.target.parentElement.remove();
+	}
+});
