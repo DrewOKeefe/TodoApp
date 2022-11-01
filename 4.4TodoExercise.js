fa838a4ccb11f755b2c195a426baa698d20952a1
Drew O'Keefe <okeefedrew@gmail.com>
2022-11-01 10:07:01 -0500

    main commit

--- 4.4TodoExercise.js
+++ 4.4TodoExercise.js
@@ -1,5 +1,5 @@
 //CREATING TODO LIST:
-const form = document.querySelector('form');
+const form = document.querySelector('#add-todo');
 const input = document.querySelector('#task');
 const todoList = document.querySelector('#todoList');
 
@@ -9,6 +9,18 @@ todoList.addEventListener('click', function (e) {
 		e.target.classList.toggle('completed');
 	}
 });
+
+//RETRIEVING FROM LOCAL STORAGE:
+// const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+// for (let i = 0; i < savedTodos.length; i++) {
+// 	let newTodo = document.createElement('li');
+// 	newTodo.innerText = savedTodos[i].task;
+// 		newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+// 		if (newTodo.isCompleted) {
+// 			newTodo.style.textDecoration = 'line-through';
+// 		}
+// 	todoList.appendChild(newTodo);
+// }
 
 //CREATING NEW LI WITH BUTTONS:
 form.addEventListener('submit', function (e) {
@@ -30,20 +42,8 @@ form.addEventListener('submit', function (e) {
 	});
 
 	//SAVE TO LOCAL STORAGE:
-	const savedTodos = todoList.innerText;
-	console.log(savedTodos);
-	localStorage.setItem('savedTodos', JSON.stringify(savedTodos));
-	// console.log(localStorage.getItem(savedTodos));
-
-	//RETRIEVING FROM LOCAL STORAGE:
-	// const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
-	// for (let i = 0; i < savedTodos.length; i++) {
-	// 	let newTodo = document.createElement('li');
-	// 	newTodo.innerText = savedTodos[i].task;
-	// 	newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
-	// 	if (newTodo.isCompleted) {
-	// 		newTodo.style.textDecoration = 'line-through';
-	// 	}
-	// 	todoList.appendChild(newTodo);
-	// }
+	// savedTodos.push({ task: newTodo.innerText, isCompleted: false });
+	localStorage.setItem('todos', JSON.stringify(taskValue));
+	console.log(localStorage.getItem('todos'));
 });
+
