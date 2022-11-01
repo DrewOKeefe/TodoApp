123a5c71c02a04641fa27fa95adbc5062e172435
Drew O'Keefe <okeefedrew@gmail.com>
2022-11-01 09:56:01 -0500

    JS work

--- 4.4TodoExercise.js
+++ 4.4TodoExercise.js
@@ -10,44 +10,34 @@ todoList.addEventListener('click', function (e) {
 	}
 });
 
-//RETRIEVING FROM LOCAL STORAGE:
-// const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
-// for (let i = 0; i < savedTodos.length; i++) {
-// 	let newTodo = document.createElement('li');
-// 	newTodo.innerText = savedTodos[i].task;
-// 		newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
-// 		if (newTodo.isCompleted) {
-// 			newTodo.style.textDecoration = 'line-through';
-// 		}
-// 	todoList.appendChild(newTodo);
-// }
-
 //CREATING NEW LI WITH BUTTONS:
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 
 	//NEW LI:
-	let newTodo = document.createElement('li');
-	let taskValue = document.getElementById('task').value;
-	newTodo.innerText = taskValue;
-	input.value = '';
-	todoList.appendChild(newTodo);
-
+	if(input.value) {
+		let newTodo = document.createElement('li');
+		let taskValue = document.getElementById('task').value;
+		newTodo.innerText = taskValue;
+		input.value = '';
+		todoList.appendChild(newTodo);
+	
+	
 	//NEW DELETE BUTTON:
-	// let deleteBtn = document.createElement('button');
-	// deleteBtn.innerText = 'Delete';
-	// newTodo.appendChild(deleteBtn);
-	// deleteBtn.addEventListener('click', function (e) {
-	// 	e.target.parentElement.remove();
-	// });
+	let deleteBtn = document.createElement('button');
+	deleteBtn.innerText = 'Delete';
+	newTodo.appendChild(deleteBtn);
+	deleteBtn.addEventListener('click', function (e) {
+		e.target.parentElement.remove();
+	});
+} else alert("Please enter task");
 
 	//SAVE TO LOCAL STORAGE:
-	const savedTodos = todoList.innerText;
-	console.log(savedTodos);
-	localStorage.setItem('savedTodos', JSON.stringify(savedTodos));
-	// console.log(localStorage.getItem(savedTodos));
+	localStorage.setItem('savedTodos', todoList.innerHTML);
 });
 
+//RETRIEVING FROM LOCAL STORAGE:
+todoList.innerHTML = localStorage.getItem('savedTodos');
 
 // const list = document.querySelector("#list");
 // const textInput = document.querySelector("#listIn");
